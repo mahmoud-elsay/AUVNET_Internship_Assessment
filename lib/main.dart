aad2940d318b13ef4bc5cd32df1b2dc19e8ec2b8
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-06-16 15:15:20 +0300

    native splash and fix android 12 problem

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:nawel/core/routing/routes.dart';
 import 'package:nawel/core/routing/app_router.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 2));
   runApp(Nawel(appRoutes: AppRouter(), initialRoute: Routes.onBoardingScreen));
 }
