bc2c2af425082f797812dfc85e22199fb07f700f
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-06-16 12:25:15 +0300

    Add flutter_native_splash configuration and create onboarding screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:nawel/core/routing/routes.dart';
+import 'package:nawel/features/onboarding/onboarding_screen.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
-    //this arguments to be passed in any screen like this ( arguments as ClassName )
-    final arguments = settings.arguments;
-
     switch (settings.name) {
-      // case Routes.onBoardingScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => const OnboardingScreen(),
-      //   );
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
       // case Routes.loginScreen:
       //   return MaterialPageRoute(
       //     builder: (_) => const LoginScreen(),
