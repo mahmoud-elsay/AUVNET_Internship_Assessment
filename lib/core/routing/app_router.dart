72e5b74f6d11c0b823be29a4bb9a31aaeddc1e20
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-06-17 05:45:51 +0300

    sign up logic done

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:nawel/core/di/di.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nawel/core/routing/routes.dart';
 import 'package:nawel/features/auth/login/ui/login_screen.dart';
 import 'package:nawel/features/onboarding/onboarding_screen.dart';
 import 'package:nawel/features/auth/sign_up/ui/sign_up_screen.dart';
+import 'package:nawel/features/auth/sign_up/logic/sign_up_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
-    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    // Arguments to be passed in any screen like this ( arguments as ClassName )
     final arguments = settings.arguments;
+
     switch (settings.name) {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+
       case Routes.loginScreen:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
+
       case Routes.signUpScreen:
-        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider<SignUpBloc>(
+                create: (context) => getIt<SignUpBloc>(),
+                child: const SignUpScreen(),
+              ),
+        );
 
       default:
         return MaterialPageRoute(
